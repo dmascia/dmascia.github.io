d1150440cfbf41a5d5897391221468c5bb307d45
David Mascia <753866+dmascia@users.noreply.github.com>
2024-12-20 14:48:35 -0500

    Updates

---
+++ precache-manifest.ab5c771a190ff52b79adc4841028d207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f269fc1e3268890bcd5c6caeadb40033",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "f7cec191a85249475a55",
+    "url": "/react-frontend-dev-portfolio/static/css/2.bee5c351.chunk.css"
+  },
+  {
+    "revision": "904247eb7438b627735a",
+    "url": "/react-frontend-dev-portfolio/static/css/main.05cb5486.chunk.css"
+  },
+  {
+    "revision": "f7cec191a85249475a55",
+    "url": "/react-frontend-dev-portfolio/static/js/2.690c5737.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/react-frontend-dev-portfolio/static/js/2.690c5737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904247eb7438b627735a",
+    "url": "/react-frontend-dev-portfolio/static/js/main.2ba34e45.chunk.js"
+  },
+  {
+    "revision": "0c71dbb3b6aa4f86725d",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.89d7af9d.js"
+  }
+]);
